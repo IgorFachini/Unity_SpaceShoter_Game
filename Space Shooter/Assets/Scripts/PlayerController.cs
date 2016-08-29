a05e41f9206a180310845762015dfd39ca18a487
Igor Fachini <igor.fachini@catolicasc.org.br>
2016-08-29 13:45:08 -0300

    Up project, steps finished(02,03,04,05).
    02 - The player GameObject
    03 - Camera and lighting
    04 - Adding a background
    05 - Moving the player

---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Velocidade da nave
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        //Limite da caixa do espaço.
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        //Rotação da nave em movimento.
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
